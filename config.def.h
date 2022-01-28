c85f27dbb732412e5c1733bfd526ea7f24046fee
Augusto Fraga Giachero <afg@augustofg.net>
2022-01-28 14:19:49 -0300

    Modify font type, size and foreground color

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -120,7 +120,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
+	"green", /* default foreground colour */
 	"black", /* default background colour */
 };
 
